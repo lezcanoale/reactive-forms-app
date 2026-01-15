d71d62d7fc6a4db79cb17e7f35d4acd4eb0fa028
Alejandra Lezcano <ma.alexa2000@fpuna.edu.py>
2026-01-15 09:31:13 -0300

    feat: implement reactive form with validation for gender, notifications, and terms

--- src/app/reactive/pages/swtiches-page/swtiches-page.ts
+++ src/app/reactive/pages/swtiches-page/swtiches-page.ts
@@ -1,9 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-swtiches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './swtiches-page.html',
 })
-export class SwtichesPageComponent {}
+export class SwtichesPageComponent {
+  fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    gender: ['F', [Validators.required]],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
